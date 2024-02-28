ccbf89c5af27245a16110407e408f62dac220147
Alessandro Palla <alessandro.palla@intel.com>
2024-02-28 14:11:03 +0000

    Remove gemma test

--- test/python/test_optimizations.py
+++ test/python/test_optimizations.py
@@ -132,6 +132,9 @@ def test_fusion(model_name, hidden_size, intermediate_size, batch, bias):
 @pytest.mark.parametrize("bias", [True, False])
 def test_model(model_name, hidden_size, intermediate_size, sequence_length, bias):
 
+    if model_name == "GemmaMLP":
+        pytest.skip("Cannot fetch model from github action")
+
     with torch.no_grad():
         model = get_model(model_name, hidden_size, intermediate_size, bias).eval()
         example_input = torch.randint(
