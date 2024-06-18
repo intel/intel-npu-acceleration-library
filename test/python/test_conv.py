aa09699bf131970f59aa8f6ce547c0c2154bda67
Alessandro Palla <alessandro.palla@intel.com>
2024-06-18 18:38:45 +0100

    Add new tests

--- test/python/test_conv.py
+++ test/python/test_conv.py
@@ -11,12 +11,29 @@ import torch
 
 
 class DummyConv(torch.nn.Module):
-    def __init__(self, in_channels, out_channels, kernels, bias, groups):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernels,
+        bias,
+        groups,
+        stride=1,
+        padding=0,
+        dilation=1,
+    ):
         super().__init__()
         if groups == -1:
             groups = out_channels
         self.conv = torch.nn.Conv2d(
-            in_channels, out_channels, kernels, bias=bias, groups=groups
+            in_channels,
+            out_channels,
+            kernels,
+            bias=bias,
+            groups=groups,
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
         )
 
     def forward(self, x):
@@ -26,11 +43,15 @@ class DummyConv(torch.nn.Module):
 @pytest.mark.parametrize("in_channels", [32, 128, 256])
 @pytest.mark.parametrize("out_channels", [32, 128, 256])
 @pytest.mark.parametrize("kernels", [1, 3])
-@pytest.mark.parametrize("dim", [16, 128])
+@pytest.mark.parametrize("dim", [16, 32])
 @pytest.mark.parametrize("bias", [True, False])
 @pytest.mark.parametrize("dtype", [torch.float16])
+@pytest.mark.parametrize("stride", [1, 2])
+@pytest.mark.parametrize("padding", [0, 1])
 @pytest.mark.parametrize("groups", [1, -1])
-def test_conv(in_channels, out_channels, kernels, dim, bias, dtype, groups):
+def test_conv(
+    in_channels, out_channels, kernels, dim, bias, dtype, stride, padding, groups
+):
     torch.manual_seed(42)
 
     if groups != 1 and in_channels != out_channels:
@@ -39,7 +60,13 @@ def test_conv(in_channels, out_channels, kernels, dim, bias, dtype, groups):
     with torch.no_grad():
         X = torch.rand((1, in_channels, dim, dim), dtype=torch.float16)
         conv = DummyConv(
-            in_channels, out_channels, kernels, bias=bias, groups=groups
+            in_channels,
+            out_channels,
+            kernels,
+            bias=bias,
+            groups=groups,
+            stride=stride,
+            padding=padding,
         ).half()
         conv.conv.weight.data *= 128
         y_ref = conv(X)
