156f61d1577b82e9ae1bdab8d730ffeb4f161c39
SarahByrneIntel <sarahbyrne@intel.com>
2024-06-19 11:06:19 +0100

    Adding support for constant operator

--- include/intel_npu_acceleration_library/nn_factory.h
+++ include/intel_npu_acceleration_library/nn_factory.h
@@ -50,27 +50,11 @@ public:
      *
      * @param dtype element type of the tensor constant
      * @param shape shape of the tensor constant
-     * @param values vector of literals for initializing the tensor constant
+     * @param dst data pointer of the tensor constant
      * @return ov::op::Op*
      */
-    template <typename T>
-    ov::op::Op* constant(ov::element::Type_t dtype, std::vector<size_t> shape, std::vector<T>& values) {
-        auto constant = std::make_shared<ov::opset1::Constant>(dtype, ov::Shape(shape), values);
-        operations.push_back(constant);
-        return constant.get();
-    }
-
-    /**
-     * @brief Create a new constant object
-     *
-     * @param dtype element type of the tensor constant
-     * @param shape shape of the tensor constant
-     * @param value value for initializing the tensor constant
-     * @return ov::op::Op*
-     */
-    template <class T, class = typename std::enable_if<std::is_fundamental<T>::value>::type>
-    ov::op::Op* constant(ov::element::Type_t dtype, std::vector<size_t> shape, T value) {
-        auto constant = std::make_shared<ov::opset1::Constant>(dtype, ov::Shape(shape), value);
+    ov::op::Op* constant(ov::element::Type_t dtype, std::vector<size_t> shape, const void* dst) {
+        auto constant = std::make_shared<ov::opset1::Constant>(dtype, ov::Shape(shape), dst);
         operations.push_back(constant);
         return constant.get();
     }
