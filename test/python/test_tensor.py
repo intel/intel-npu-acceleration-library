ed4c3e81264313a8f900a27c4ef606d11da64f05
Alessandro Palla <alessandro.palla@intel.com>
2024-07-04 14:36:32 +0100

    Fix ops and r_ops in case of float and int

--- test/python/test_tensor.py
+++ test/python/test_tensor.py
@@ -286,3 +286,43 @@ def test_reduce_operations(batch, hidden_dim, axis, op):
         assert 1 - r2_score([reference, 1], [result, 1]) < 0.01
     else:
         assert 1 - r2_score(reference, result) < 0.01
+
+
+@pytest.mark.parametrize("shape", [[1, 128, 32, 64], [12, 231]])
+@pytest.mark.parametrize("op", ["+", "-", "*", "/"])
+@pytest.mark.parametrize("side", ["left", "right"])
+@pytest.mark.parametrize("value", [3, -10])
+def test_float_op(shape, op, side, value):
+    def op_func(a, b):
+        if op == "+":
+            return a + b
+        elif op == "-":
+            return a - b
+        elif op == "*":
+            return a * b
+        elif op == "/":
+            return a / b
+
+    def act(a, b):
+        if side == "left":
+            return op_func(b, a)
+        else:
+            return op_func(a, b)
+
+    x = torch.ones(shape).to(torch.float16)
+    reference = act(x, value)
+
+    model = NNFactory()
+    t1 = model.parameter(shape, float16)
+    out = act(t1, value)
+    model.compile()
+
+    result = model(x)
+
+    assert (
+        1
+        - r2_score(
+            reference.flatten().detach().numpy(), result.flatten().detach().numpy()
+        )
+        < 0.001
+    )
