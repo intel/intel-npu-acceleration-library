5dc3068199ed96f709f0aceb30f4bd1c51f6e82c
henry8248 <henrywrb@gmail.com>
2024-07-08 15:24:28 +0800

    2nd pre-commit

--- examples/llama3_token_per_second.py
+++ examples/llama3_token_per_second.py
@@ -95,6 +95,5 @@ By making a contribution to this project, I certify that:
     maintained indefinitely and may be redistributed consistent with
     this project or the open source license(s) involved.
 
-
 Signed-off-by: Henry Wang henrywrb@gmail.com
 """
